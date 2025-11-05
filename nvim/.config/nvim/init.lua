bbb6a9f32f50421abd2e7b77297abf068a9b2559
Thinh Nguyen <thinhntr@gmail.com>
2025-11-05 09:49:41 +0700

    nvim: add keymap to move line

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -79,6 +79,9 @@ vim.keymap.set({ "n", "v" }, "<leader>C", '"_C')
 vim.keymap.set({ "n", "v" }, "<leader>y", '"+y')
 vim.keymap.set({ "n", "v" }, "<leader>Y", '"+Y')
 
+vim.keymap.set("n", "[e", "<cmd>move--<cr>", { desc = "move current line up" })
+vim.keymap.set("n", "]e", "<cmd>move+<cr>", { desc = "move current line up" })
+
 vim.keymap.set("x", ">", ">gv", { desc = "shift lines right nonstop" })
 vim.keymap.set("x", "<", "<gv", { desc = "shift lines left nonstop" })
 
